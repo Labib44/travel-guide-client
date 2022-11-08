3f4c86d32e638d0b11424ced0cf73cf241332fc2
Labib44 <mdlabib162@gmail.com>
2022-11-09 02:25:54 +0600

    Added Blog page

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -8,7 +8,7 @@ const Blog = () => {
                 <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">Frequently Asked Questions</h2>
                 <div className="grid gap-10 md:gap-8 sm:p-3 md:grid-cols-2 lg:px-12 xl:px-32">
                     <div>
-                        <h3 className="font-semibold text-2xl text-cyan-400">Lorem ipsum dolor sit amet.</h3>
+                        <h3 className="font-semibold text-2xl text-cyan-400">Difference between SQL and NoSQL.</h3>
                         <p className="mt-1 dark:text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum voluptatem consequatur ratione, doloremque debitis? Fuga labore omnis minima, quisquam delectus culpa!</p>
                     </div>
                     <div>
